f3ce10f5cf3b8b7d68ab928f120195b9c73ad892
lucas <lucas@lucas-K46CB>
2017-07-27 11:04:46 -0300

    versão 1.0.0 iniciando projeto

---
+++ LucasMoneyApi/src/main/java/com/lucas/lucasmoney/api/LucasMoneyApiApplication.java
@@ -0,0 +1,12 @@
+package com.lucas.lucasmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucasMoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucasMoneyApiApplication.class, args);
+	}
+}
